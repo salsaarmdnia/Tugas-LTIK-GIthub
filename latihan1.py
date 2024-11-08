0c85a29eb07ab3d015a1cf4232e8d55401d092d3
salsaarmdnia <ramadhaniasalsabila251@gmail.com>
2024-11-08 21:23:18 +0700

    first commit

---
+++ latihan1.py
@@ -0,0 +1,10 @@
+#Nama: Salsabila Rahma Ramadhania
+#NIM: 2409878
+#Kelas: RPL 1B
+
+#Menghitung volume tabung yang dibuat dalam sebuah fungsi
+def volume_tabung(jari_jari,tinggi):
+    print(22/7 * jari_jari**2 * tinggi)
+    
+volume_tabung(7,9)
+
