4bcdd5cb7efcc63ff3e121f00f9ec1f974d87677
salsaarmdnia <ramadhaniasalsabila251@gmail.com>
2024-11-08 21:35:58 +0700

    Update file Latihan1.py

--- latihan1.py
+++ latihan1.py
@@ -5,6 +5,7 @@
 #Menghitung volume tabung yang dibuat dalam sebuah fungsi
 def volume_tabung(jari_jari,tinggi):
     print(22/7 * jari_jari**2 * tinggi)
+    print("volume tabung")
     
 volume_tabung(7,9)
 
